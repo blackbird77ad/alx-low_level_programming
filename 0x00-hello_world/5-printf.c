ae3f384238f405f4ab13a72afb034e0060b5dd8d
blackbird77ad <dhavyb.h@gmail.com>
2023-04-20 19:24:31 -0700

    standard output with printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of a print command.
+ *
+ *Return: value returns zero (successs)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
