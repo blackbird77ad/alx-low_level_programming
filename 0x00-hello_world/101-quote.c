c5d14cb592ffbb236583e1afc47284e068d7513a
blackbird77ad <dhavyb.h@gmail.com>
2023-04-20 21:21:20 -0700

    a returning of 1 to main function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - entry point
+ *Return: value set to 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is usefu\"
+- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
