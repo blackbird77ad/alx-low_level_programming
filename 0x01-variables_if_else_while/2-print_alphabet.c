6583df75efb26dee2a1f56e8573c16c2392fa816
blackbird77ad <dhavyb.h@gmail.com>
2023-04-21 18:17:29 -0700

    putchar usage

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
