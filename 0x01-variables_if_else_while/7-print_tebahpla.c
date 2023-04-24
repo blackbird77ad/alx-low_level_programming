9c511271ecab7a9e7763ff6986abfffa56f1f7ce
blackbird77ad <dhavyb.h@gmail.com>
2023-04-24 14:30:33 -0700

    Numberz task 6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n '==';
+	}
+	putchar('\n');
+	return (0);
+}
