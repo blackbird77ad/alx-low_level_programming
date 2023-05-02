79e457bec8d6e0e6261b9b50d2bca4d7058efe37
blackbird77ad <dhavyb.h@gmail.com>
2023-05-02 12:22:01 -0700

    task_0-0. 98 Battery st

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ *main - tests function that prints if integer is positive or negative
+ * Return: Always zero
+ */
 
-int main(void)
+void positive_or_negative(int i);
 {
-	int i;
-
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
