4916ad71d2b79f4b4515dc214687f0b4e3beb030
blackbird77ad <dhavyb.h@gmail.com>
2023-05-02 14:43:40 -0700

    task_4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_rev -print a string in reverse order.
+ *@s: the string to be reversed.
+ *Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}
