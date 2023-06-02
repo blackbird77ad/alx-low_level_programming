91728feb4cef36393eb66fb706c85650045d3911
blackbird77ad <dhavyb.h@gmail.com>
2023-06-01 17:28:44 -0700

    linked list-data structure

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,11 @@
  */
 void myStartupFun(void) __attribute__ ((constructor));
 
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\tI bore my house upon
-my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
